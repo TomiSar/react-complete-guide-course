d31f771881059970c78514e046ad43bbe01254cc
Tomi Sarjamo <tomi.sarjamo@tietoevry.com>
2025-08-14 14:28:35 +0300

    Added some finishing touches to event sample project.

--- 21b_Routing/frontend/src/pages/HomePage.jsx
+++ 21b_Routing/frontend/src/pages/HomePage.jsx
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom';
+import PageContent from '../components/PageContent';
 
 function HomePage() {
   return (
     <>
-      <h1>Home Page</h1>
+      <PageContent title='Welcome!'>
+        <p>
+          Browse all our amazing events or start by adding your own event to the
+          list.
+        </p>
+      </PageContent>
       <p>
         Go to <Link to='/events'>events page</Link>
       </p>
