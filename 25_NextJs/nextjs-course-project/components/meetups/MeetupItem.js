9b07dc8c893e280354e1ee46d9cac95fd6dc9b43
Tomi Sarjamo <tomi.sarjamo@tietoevry.com>
2025-08-20 21:43:51 +0300

    Added some small sample project and the actual NextJs course project with some components and pages (Soon to be updated)..

---
+++ 25_NextJs/nextjs-course-project/components/meetups/MeetupItem.js
@@ -0,0 +1,30 @@
+import { useRouter } from 'next/router';
+import Card from '../ui/Card';
+import classes from './MeetupItem.module.css';
+
+function MeetupItem(meetup) {
+  const router = useRouter();
+
+  function showDetailsHandler() {
+    router.push('/' + meetup.id);
+  }
+
+  return (
+    <li className={classes.item}>
+      <Card>
+        <div className={classes.image}>
+          <img src={meetup.image} alt={meetup.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{meetup.title}</h3>
+          <address>{meetup.address}</address>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={showDetailsHandler}>Show Details</button>
+        </div>
+      </Card>
+    </li>
+  );
+}
+
+export default MeetupItem;
---
+++ 25_NextJs/nextjs-course-project/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
+      title='First Meetup'
+      address='Meetup street 1, 12345 Meetup City'
+      description='The meetup description'
+    />
+  );
+}
+
+export default MeetupDetails;
